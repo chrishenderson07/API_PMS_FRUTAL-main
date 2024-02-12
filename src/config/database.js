9e7ea6a1726438b8122bb41b369d1e3cdfb9dcaa
chrishenderson07 <chris.hend012@gmail.com>
2024-02-12 11:27:32 -0300

    novos ajustes

--- src/config/database.js
+++ src/config/database.js
@@ -1,60 +1,44 @@
-let ambiente = undefined;
+let ambiente = undefined
 
-switch(process.env.PUBLICAR){
-    case 'HML': 
-        ambiente = configurarHML();
-        break;
-    case 'PRD':
-        ambiente = configurarPRD();
-        break;
-    default:
-        ambiente = configurarLOCAL();
+switch (process.env.PUBLICAR) {
+	case 'HML':
+		ambiente = configurarHML()
+		break
+	case 'PRD':
+		ambiente = configurarPRD()
+		break
+	default:
+		ambiente = configurarLOCAL()
 }
 
-function configurarHML(){
-    return {
-        dialect: process.env.HML_DIALECT,
-        host: process.env.HML_HOST,
-        port: process.env.HML_PORT,
-        username: process.env.HML_USER_NAME,
-        password: process.env.HML_PASSWORD,
-        database: process.env.HML_DATABASE,
-        define: {
-            timestamps: true,
-            underscored: true,
-        }
-    }
+function configurarHML() {
+	return {
+		host: process.env.HML_HOST,
+		port: process.env.HML_PORT,
+		username: process.env.HML_USER_NAME,
+		password: process.env.HML_PASSWORD,
+		database: process.env.HML_DATABASE,
+	}
 }
 
-function configurarPRD(){
-    return {
-        dialect: process.env.PRD_DIALECT,
-        host: process.env.PRD_HOST,
-        port: process.env.PRD_PORT,
-        username: process.env.PRD_USER_NAME,
-        password: process.env.PRD_PASSWORD,
-        database: process.env.PRD_DATABASE,
-        define: {
-            timestamps: true,
-            underscored: true,
-        }
-    }
+function configurarPRD() {
+	return {
+		host: process.env.PRD_HOST,
+		port: process.env.PRD_PORT,
+		username: process.env.PRD_USER_NAME,
+		password: process.env.PRD_PASSWORD,
+		database: process.env.PRD_DATABASE,
+	}
 }
 
-function configurarLOCAL(){
-    return {
-        dialect: process.env.LOCAL_DIALECT,
-        host: process.env.LOCAL_HOST,
-        port: process.env.LOCAL_PORT,
-        username: process.env.LOCAL_USER_NAME,
-        password: process.env.LOCAL_PASSWORD,
-        database: process.env.LOCAL_DATABASE,
-        define: {
-            timestamps: true,
-            underscored: true,
-        }
-    }
-    
+function configurarLOCAL() {
+	return {
+		host: process.env.LOCAL_HOST,
+		port: process.env.LOCAL_PORT,
+		username: process.env.LOCAL_USER_NAME,
+		password: process.env.LOCAL_PASSWORD,
+		database: process.env.LOCAL_DATABASE,
+	}
 }
 
-module.exports = ambiente;
+module.exports = ambiente
