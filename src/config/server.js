a2b19f03c23a35b857d33cae39167886d3e092a7
chrishenderson07 <chris.hend012@gmail.com>
2024-02-12 11:28:47 -0300

    new adjusts

--- src/config/server.js
+++ src/config/server.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
-require('../database');
+require('dotenv').config()
+require('../database')
 
-const express = require('express');
-const cors = require('cors');
-const routes = require('../../routes');
+const express = require('express')
+const cors = require('cors')
+const routes = require('../../routes')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(routes);
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-module.exports = app;
-
+module.exports = app
