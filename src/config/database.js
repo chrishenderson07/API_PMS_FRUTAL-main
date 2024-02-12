12b546ccb3a136994b1dbc41b9032a423a32e2b2
chrishenderson07 <chris.hend012@gmail.com>
2024-02-12 11:36:48 -0300

    new adjusts 2

--- src/config/database.js
+++ src/config/database.js
@@ -13,31 +13,46 @@ switch (process.env.PUBLICAR) {
 
 function configurarHML() {
 	return {
+		dialect: process.env.HML_DIALECT,
 		host: process.env.HML_HOST,
 		port: process.env.HML_PORT,
 		username: process.env.HML_USER_NAME,
 		password: process.env.HML_PASSWORD,
 		database: process.env.HML_DATABASE,
+		define: {
+			timestamps: true,
+			underscored: true,
+		},
 	}
 }
 
 function configurarPRD() {
 	return {
+		dialect: process.env.PRD_DIALECT,
 		host: process.env.PRD_HOST,
 		port: process.env.PRD_PORT,
 		username: process.env.PRD_USER_NAME,
 		password: process.env.PRD_PASSWORD,
 		database: process.env.PRD_DATABASE,
+		define: {
+			timestamps: true,
+			underscored: true,
+		},
 	}
 }
 
 function configurarLOCAL() {
 	return {
+		dialect: process.env.LOCAL_DIALECT,
 		host: process.env.LOCAL_HOST,
 		port: process.env.LOCAL_PORT,
 		username: process.env.LOCAL_USER_NAME,
 		password: process.env.LOCAL_PASSWORD,
 		database: process.env.LOCAL_DATABASE,
+		define: {
+			timestamps: true,
+			underscored: true,
+		},
 	}
 }
 
